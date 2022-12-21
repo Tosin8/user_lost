c411c90cd843ff1ee8a520ea8b2072d6a98f2be9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-22 00:44:41 +0100

    Creating Components

--- lib/404_page/404_page.dart
+++ lib/404_page/404_page.dart
@@ -1,30 +1,3 @@
-// ignore: file_names
 import 'package:flutter/material.dart'; 
 
-class 404_error_page extends StatefulWidget {
-  const 404_error_page();
-
-  @override
-  State<404_error_page> @override
-  createState() => _404_error_pageState();
-}
-
-class _404_error_pageState extends State<404_error_page> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      appBar: AppBar(), 
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween, 
-          children: [
-          Image.asset(), 
-          Text('Products'), 
-          Text('About'), 
-          Text('Blog'), 
-          Text('Contacts'), 
-        ]), 
-      )
-    );
-  }
-}
+st
