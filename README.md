2aadaa9c680f8217612a7b919c7b4d0d619afd1c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-22 00:23:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# user_lost
+
+A 1 page Flutter website project showin 404 error. 
+
+Demo: 
---
+++ android/app/src/main/kotlin/com/example/user_lost/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.user_lost
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
